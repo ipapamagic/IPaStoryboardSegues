351994dafce386199018704c6a3763ccfddd4e1d
IPa Chen <ipapamagic@gmail.com>
2020-10-30 17:14:15 +0800

    support ios 14

--- IPaStoryboardSegues/Classes/IPaReplaceRootSegue.swift
+++ IPaStoryboardSegues/Classes/IPaReplaceRootSegue.swift
@@ -10,10 +10,24 @@ import UIKit
 
 open class IPaReplaceRootSegue: UIStoryboardSegue {
     override open func perform() {
-        guard let oWindow = UIApplication.shared.delegate?.window else {
-            return
+        var window:UIWindow
+        if #available(iOS 13.0, *) {
+            guard let oWindow = UIApplication.shared.connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .map({$0 as? UIWindowScene})
+                .compactMap({$0})
+                .first?.windows
+                    .filter({$0.isKeyWindow}).first else {
+                return
+            }
+            window = oWindow
+        } else {
+            guard let oWin = UIApplication.shared.delegate?.window,let oWindow = oWin else {
+                return
+            }
+            window = oWindow
         }
-        let window = oWindow!
+        
         guard let oldVC = window.rootViewController else {
             window.rootViewController = self.destination
             return;
